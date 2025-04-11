fd38d2281214c33f345d1f8bcf3b529e7c1d28e8
Imanol Asolo <jjusturi@gmail.com>
2025-04-11 11:21:45 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',  # Django REST framework
     'accounts',  # Custom accounts app
+    'aktos-project.onrender.com', 'www.aktos-project.onrender.com' # For RENDER deploy
     
 ]
 
